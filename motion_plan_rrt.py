f911a08699237758a01dff54b0cd96ca8c0b38d9
kuubi3d-udacity <kuubi3d@gmail.com>
2021-01-28 13:49:01 -1000

    update

--- motion_plan_rrt.py
+++ motion_plan_rrt.py
@@ -61,6 +61,7 @@ class MotionPlanning(Drone):
             if -1.0 * self.local_position[2] > 0.95 * self.target_position[2]:
                 self.waypoint_transition()
         elif self.flight_state == States.WAYPOINT:
+            print (np.linalg.norm(self.local_velocity[0:2]))
             if np.linalg.norm(self.target_position[0:2] - self.local_position[0:2]) < 1.0:
                 if len(self.waypoints) > 0:
                     self.waypoint_transition()
